d1ba8f372f36f9f67d0e1fd4dd12b8c0f3204cae
Shay peduim <shay.peduim@gmail.com>
2025-12-14 15:59:47 +0200

    add result then

--- AnotherResult/Result.cs
+++ AnotherResult/Result.cs
@@ -1,4 +1,6 @@
 
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 public class Result
 {
   public static Result<V> Success<V>(V value = default, string message = "Success")
@@ -10,6 +12,26 @@ public class Result
   {
     return new Result<V>(default(V), error, message);
   }
+
+  public static Result<bool> And<T1, T2>(Result<T1> one, Result<T2> two)
+  {
+    return one.isSuccess && two.isSuccess ? true : Result.Fail<bool>(new Error("At least one result not Success"));
+  }
+
+  public static Result<bool> Or<T1, T2>(Result<T1> one, Result<T2> two)
+  {
+    return one.isSuccess || two.isSuccess ? true : Result.Fail<bool>(new Error("No results in Success"));
+  }
+
+  public static Result<bool> All(params Result[] results)
+  {
+    return results.All(r => r.isSuccess) ? true : Result.Fail<bool>(new Error("Not all results are Success"));
+  }
+
+  public string message;
+  public Error error = null;
+  public bool isSuccess => error == null;
+  public bool hasError => error != null;
 }
 
 public class Result<V>
@@ -30,10 +52,22 @@ public class Result<V>
 
   public static implicit operator Result<V>(V val) => new Result<V>(val, null, "Success");                                                  // implicit cast from object to Result
   public static implicit operator Result<V>(Error error) => new Result<V>(default(V), error, "Error");                                      // implicit cast from Error to Result
+  public static implicit operator Result(Result<V> val) => new Result() { error = val.error , message = val.message };
 
-  public void Match(Action successAction = null, Action failAction = null)
+  public Result<V> Match(Action<Result<V>> successAction = null, Action<Result<V>> failAction = null)
   {
-    if (isSuccess) { if (successAction != null) successAction(); } else { if (failAction != null) failAction(); };
+    (isSuccess ? successAction : failAction)?.Invoke(this);
+    return this;
+  }
+
+  public Result<T> Then<T>(Func<V, Result<T>> func)
+  {
+    return isSuccess ? func(value) : new Result<T>(default(T), error, message);
+  }
+
+  public Result<T> Convert<T>(Func<V, T> func)
+  {
+    return isSuccess ? new Result<T>(func(value), null, message) : new Result<T>(default(T), error, message);
   }
 
   public V OrDefault(V def)
