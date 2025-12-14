2d04f49dec8e5ae7336ccde6dd1a4f44951ce6fb
shay peduim <shay.yosefpeduim@philips.com>
2025-12-14 15:58:23 +0200

    add return then

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -5,10 +5,11 @@ using Sample;
 // Another Result Sample
 Console.WriteLine("Another Result Sample");
 
-Service service = new Service();
+UserService userService = new UserService();
+PasswordService passwordService = new PasswordService();
 
 // get "real" user
-Result<User> result = service.GetUser(10);
+Result<User> result = userService.Get(10);
 result.OrDefault(new User()).ID = 123;
 Console.WriteLine($"get user 10 result is {result}");
 
@@ -18,14 +19,14 @@ if (result.OrDefault(null) != null)
 }
 
 // get null object user
-result = service.GetUser(0);
+result = userService.Get(0);
 result.OrDefault(new User()).ID = 10;
 Console.WriteLine($"get user 0 result is {result}");
 
-result.Match(() => Console.WriteLine($"get user 0 result is success") );
+result.Match((success) => Console.WriteLine($"get user 0 result is success") );
 
 // get error
-result = service.GetUser(-10);
+result = userService.Get(-10);
 result.OrDefault(new User()).ID = 10;
 Console.WriteLine($"get user -10 result is {result}");
 
@@ -35,7 +36,7 @@ if (result.hasError)
 }
 
 // get exception error
-result = service.GetUser(-1000);
+result = userService.Get(-1000);
 result.OrDefault(new User()).ID = 10;
 Console.WriteLine($"get user -1000 result is {result}");
 
@@ -43,6 +44,31 @@ if (result.hasError)
 {
   Console.WriteLine($"get user -1000 returned an error {result.error}");
 }
+
+Result.And(userService.Get(10), userService.Save(new User()))
+  .Match(
+    successAction: (res) => Console.WriteLine($"Or result is success"),
+    failAction: (res) => Console.WriteLine($"Or result is fail")
+  );
+
+Result.All(userService.Get(10), userService.Get(-10), userService.Validate(new User()), userService.Save(new User()))
+  .Match(
+    successAction: (res) => Console.WriteLine($"Or result is success"),
+    failAction: (res) => Console.WriteLine($"Or result is fail")
+  );
+
+var newUser = userService.Create(5);
+
+var resultID =
+newUser
+  .Then(user => userService.Validate(user))
+  .Then(user => passwordService.Validate(newUser.value.Password))
+  .Then(success => userService.Save(newUser.value))
+  .Match(
+    successAction: (res) => Console.WriteLine($"user created and saved {res.value}"),
+    failAction: (res) => Console.WriteLine($"error creating user: {res.error}")
+  )
+  .Convert(user => user.ID);
 
 
 // Another Extentions sample
--- Sample/Service.cs
+++ Sample/Service.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace Sample
 {
-  public class User
+  public record User
   {
     public int ID { get; set; } = 0;
+    public string Password { get; set; } = "";
 
     public override string ToString()
     {
@@ -16,9 +17,9 @@ namespace Sample
     }
   }
 
-  public class Service
+  public class UserService
   {
-    public Result<User> GetUser(int ID)
+    public Result<User> Get(int ID)
     {
       try
       {
@@ -54,6 +55,42 @@ namespace Sample
       }
 
       return Result.Success<User>(null);
+    }
+
+    public Result<User> Validate(User user)
+    {
+      if (user.ID <= 0)
+      {
+        return new Error("ID must be greater than zero", 201);
+      }
+      return user;
+    }
+
+    public Result<User> Save(User user)
+    {
+      if (user == null)
+      {
+        return new Error("User is null", 203);
+      }
+      // simulate save
+      return Result.Success(user, "User saved");
+    }
+
+    public Result<User> Create(int id)
+    {
+      return new User() { ID = id };
+    }
+  }
+
+  public class PasswordService
+  {
+    public Result<string> Validate(string password)
+    {
+      if (string.IsNullOrEmpty(password) || password.Length < 6)
+      {
+        return new Error("Password must be at least 6 characters", 301);
+      }
+      return Result.Success(password, "Valid password");
     }
   }
 }
